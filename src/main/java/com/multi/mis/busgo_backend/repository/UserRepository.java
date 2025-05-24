0951d85425ab61b4846597d65c155a1c64dbbea1
Byukusenge-Andrew <andrewbyukusenge4@gmail.com>
2025-05-24 08:38:54 +0200

    updated user repository

--- src/main/java/com/multi/mis/busgo_backend/repository/UserRepository.java
+++ src/main/java/com/multi/mis/busgo_backend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.multi.mis.busgo_backend.repository;
 
 import com.multi.mis.busgo_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long>
     Optional<User> findByEmail(String email);
 
     List<User> findByRole(String role);
+
+    @Query("select u from User u where u.isActive = true ")
     List<User> findByIsActiveTrue();
 } 
